199f0c8c3c37d5b06ce17edc2c6d574f18139c6b
ed wassermann <vis0vis@gmail.com>
2013-11-28 14:31:15 +0100

    Corrects small mistake refactoring #twitter_handle

--- spec/models/interviewee_spec.rb
+++ spec/models/interviewee_spec.rb
@@ -18,21 +18,31 @@ describe Interviewee  do
 	context '#twitter_handle' do
 
 		it 'responds to #twitter_handle' do
-			interviewee = create(:interviewee,twitter_handle: '@steve_huffmann')
+			interviewee = interviewee_has_twitter_handle
 			expect(interviewee).to respond_to :twitter_handle
 		end
 
 		it 'returns the correct twitter_handle' do
-			interviewee = Interviewee.new(twitter_handle: '@steve_huffmann')
+			interviewee = interviewee_with_twitter_handle '@steve_huffmann'
 			expect(interviewee.twitter_handle).to eq '@steve_huffmann'
 		end
 	end
+
+
+	
 
 end
 
 
 
 
+def interviewee_has_twitter_handle
+	interviewee_with_twitter_handle '@steve_huffmann'
+end
+
+def interviewee_with_twitter_handle(twitter_handle)
+	create(:interviewee, twitter_handle: twitter_handle)
+end
 
 def named_interviewee
 	interviewee_named 'Steve Huffmann'
