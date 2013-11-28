4fbdad720e9957b4a075eddc09445a2f28627636
ed wassermann <vis0vis@gmail.com>
2013-11-28 17:29:07 +0100

    Tests response of #description on Video

---
+++ spec/factories/videos.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+	factory :video do
+    title 'My awesome title'
+		description 'Once upon a time'
+	end
+end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,12 +5,12 @@ describe Video do
 	context '#title' do
 
 		it 'responds to title' do
-      video = Video.new(title: "Stanford is at the heart of the ecosystem")
+			video = video_with_title
 			expect(video).to respond_to :title
 		end
 
 		it 'returns the correct title' do
-      video = Video.new(title: "Stanford is at the heart of the ecosystem")
+      video = video_has_title "Stanford is at the heart of the ecosystem"
 			expect(video.title).to eq "Stanford is at the heart of the ecosystem"
 		end
 	end
@@ -18,13 +18,30 @@ describe Video do
 	context '#description' do
 
 		it 'responds to description' do
-      video = Video.new(description: "This video explains the stuff")
+      video = video_with_description
 			expect(video).to respond_to :description
 		end
 
 		it 'returns the correct description' do
-      video = Video.new(description: "This video explains the stuff")
-			expect(video.description).to eq "This video explains the stuff"
+      video = video_has_description 'This video explains the stuff'
+			expect(video.description).to eq 'This video explains the stuff'
 		end
 	end
 end
+
+def video_with_description
+	video_has_description 'Some awesome description'
+end
+
+def video_has_description description
+	create(:video, description: description)
+end
+
+def video_with_title
+	video_has_title 'Some awesome title'
+end
+
+def video_has_title(title)
+	video = create(:video, title: title)
+end
+
