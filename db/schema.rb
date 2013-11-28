3238330d994dedc96c9af49811739fc477429758
ed wassermann <vis0vis@gmail.com>
2013-11-28 16:55:38 +0100

    Adds description attribute to Video

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131128153325) do
+ActiveRecord::Schema.define(version: 20131128155001) do
 
   create_table "interviewees", force: true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20131128153325) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
 end
