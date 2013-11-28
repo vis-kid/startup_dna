64001e413c9dc6c9b3a2f0c83aa776cef0dd37cb
ed wassermann <vis0vis@gmail.com>
2013-11-28 14:08:05 +0100

    Small refactor w/ contexts for interviewee_spec

--- spec/models/interviewee_spec.rb
+++ spec/models/interviewee_spec.rb
@@ -1,37 +1,37 @@
 require 'spec_helper'
 
-describe Interviewee, '#name' do
+describe Interviewee  do
 
-	it 'responds to #name' do
-		interviewee = named_interviewee	
-		expect(interviewee).to respond_to :name
+	context '#name' do
+
+		it 'responds to #name' do
+			interviewee = named_interviewee	
+			expect(interviewee).to respond_to :name
+		end
+
+		it 'returns the correct name' do
+			interviewee = interviewee_named 'Steve Huffmann'
+			expect(interviewee.name).to eq'Steve Huffmann'
+		end
 	end
 
-  it 'returns the correct name' do
-    interviewee = interviewee_named 'Steve Huffmann'
-		expect(interviewee.name).to eq'Steve Huffmann'
-	end
+	context '#twitter_handle' do
 
+		it 'responds to #twitter_handle' do
+			interviewee = create(:interviewee,twitter_handle: '@steve_huffmann')
+			expect(interviewee).to respond_to :twitter_handle
+		end
 
-  it 'responds to #twitter_handle' do
-		interviewee = interviewee_with_twitter_handle 
-		expect(interviewee).to respond_to :twitter_handle
-	end
-
-  it 'returns the correct twitter_handle' do
-    interviewee = Interviewee.new(twitter_handle: '@steve_huffmann')
-		expect(interviewee.twitter_handle).to eq '@steve_huffmann'
+		it 'returns the correct twitter_handle' do
+			interviewee = Interviewee.new(twitter_handle: '@steve_huffmann')
+			expect(interviewee.twitter_handle).to eq '@steve_huffmann'
+		end
 	end
 
 end
 
-def interviewee_has_twitter_handle(twitter_handle)
-  create(interviewee, twitter_handle: twitter_handle)
-end
 
-def interviewee_with_twitter_handle 
-  create(:interviewee, twitter_handle: '@steve_huffmann')
-end
+
 
 
 def named_interviewee
@@ -39,7 +39,7 @@ def named_interviewee
 end
 
 def interviewee_named(name)
-  create(:interviewee, name: name)
+	create(:interviewee, name: name)
 end
 
 
