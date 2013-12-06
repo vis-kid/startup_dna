8dfc638a2eaaffe0deb03625831e3ce7f783c902
ed wassermann <vis0vis@gmail.com>
2013-12-06 17:49:43 +0100

    Tests Videos page for right header,quote,title,video_title

--- spec/integration/view_videos_page_spec.rb
+++ spec/integration/view_videos_page_spec.rb
@@ -4,11 +4,34 @@ feature 'View the videos page' do
 
 	scenario 'User sees header "NEW VIDEO"' do
 		visit videos_path
-		expect(page).to have_css "[data-role='page_header']"
-		expect(page).to have_content 'New Video'
 	end
 
-	scenario 'User sees video timeline with video#title' do
+	scenario 'user sees correct title' do
+		user_sees_correct_page_title 'Videos'
+	end
+
+	scenario 'user sees header "new video"' do
+		element_has_correct_data_role 'page_header'
+		user_sees_content 'New Video'
+	end
+
+	scenario 'User sees quote of videos' do
+		video_with_quote = create(:video, quote: 'I have a quote')
+		visit videos_path
+
+		element_has_correct_data_role 'video_quote'
+		user_sees_content 'I have a quote'
+	end
+
+	scenario 'User sees title of videos' do
+		video_with_quote = create(:video, title: 'I have a title')
+		visit videos_path
+
+		element_has_correct_data_role 'video_title'
+		user_sees_content 'I have a title'
+	end
+
+	scenario 'User sees video timeline' do
     video01 = video_with_title 'First video'
     video02 = video_with_title 'Second video'
 		visit videos_path
