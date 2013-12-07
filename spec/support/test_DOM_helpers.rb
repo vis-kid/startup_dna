6c97bcf5020c6644822d942c280d6c68f32f7ec5
ed wassermann <vis0vis@gmail.com>
2013-12-07 15:32:25 +0100

    Extracts view_video_spec helpers->test_Dom_helpers

--- spec/support/test_DOM_helpers.rb
+++ spec/support/test_DOM_helpers.rb
@@ -5,12 +5,37 @@ module DomHelpers
 	end
 
 	def element_has_correct_data_role(data_role)
-		expect(page).to have_css "[data-role= #{data_role} ]"
+		expect(page).to have_css "[ data-role=#{data_role} ]"
 	end
 
 	def user_sees_content(content)
     expect(page).to have_content content
 	end
+
+	def user_does_not_see_content(content)
+    expect(page).not_to have_content content
+	end
+
+	def interviewee_named(name)
+		create :interviewee, name: name
+	end
+
+	def basic_interviewee
+		create :interviewee
+	end
+
+	def basic_video
+		create :video
+	end
+
+	def video_with_quote(quote)
+    create :video, quote: quote
+	end
+
+	def video_with_category(category)
+    create :video, category: category
+	end
+
 end
 
 RSpec.configure do  |config| 
