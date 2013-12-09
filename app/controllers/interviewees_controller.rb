f5e1255a868328922ad619831d8d3c31b8f52349
ed wassermann <vis0vis@gmail.com>
2013-12-09 14:14:13 +0100

    Tests interviewee/index for list of interviewees

--- app/controllers/interviewees_controller.rb
+++ app/controllers/interviewees_controller.rb
@@ -1,6 +1,6 @@
 class IntervieweesController < ApplicationController
 
 	def index
-
+    @interviewees = Interviewee.all
 	end
 end
--- spec/integration/view_interviewees_page_spec.rb
+++ spec/integration/view_interviewees_page_spec.rb
@@ -15,5 +15,23 @@ feature 'View the interviewees page' do
 		user_sees_content 'Interviewees'
 	end
 
+	scenario 'User sees list of interviewees' do
+		interviewee01 = interviewee_named 'David J. Phillips'
+		interviewee02 = interviewee_named 'Christian Fernandez'
+    
+		visit interviewees_path
 
+    user_sees_content 'David J. Phillips'
+    user_sees_content 'Christian Fernandez'
+		page_loads_correct_amount_of_interviewees 2
+		page_does_not_load_incorrect_amount_of_interviewees 1
+
+	end
+
+def page_loads_correct_amount_of_interviewees count
+	expect(page).to have_css 'li.interviewee', count: count
+end
+
+def page_does_not_load_incorrect_amount_of_interviewees count 
+	expect(page).not_to have_css 'li.interviewee', count: count
 end
