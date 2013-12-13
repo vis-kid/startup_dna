dfaa6e8c98ce7a4803d64a0970857c7610560e0e
ed wassermann <vis0vis@gmail.com>
2013-12-13 17:55:23 +0100

    Adds tests for Interviewee upload

--- app/models/interviewee.rb
+++ app/models/interviewee.rb
@@ -1,5 +1,5 @@
 class Interviewee < ActiveRecord::Base
 	has_many :videos
 
-	has_attached_file :user_pic
+	has_attached_file :user_pic,:styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
---
+++ spec/integration/create_new_interviewee_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature 'View the interviewee/new page' do
+
+	scenario 'create a new interviewee' do
+		visit new_interviewee_path
+
+		fill_in 'Name',           with: 'Sebastian Thrun'
+		fill_in 'About text',     with: 'Sebastian is well known for ...'
+		fill_in 'Twitter handle', with: '@thrun'
+		fill_in 'Weblink',        with: 'www.udacity.com'
+
+		click_button 'Save Interviewee'
+
+		within("[data-role='interviewee_name']") do
+			expect(page).to have_content 'Sebastian Thrun'
+		end
+	end
+end
