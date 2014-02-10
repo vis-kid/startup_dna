f28bb591aed0cfa162ca08df838c0e59343d418c
ed wassermann <vis0vis@gmail.com>
2014-02-10 21:10:46 +0100

    assets initialize_on_precompile = false in application.rb

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module StartupDna
     # config.i18n.default_locale = :de
 		#
 		config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+		config.assets.initialize_on_precompile = false
   end
 end
