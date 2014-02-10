b5c7dcd4c641d6107739be835a166dcf424a8b79
ed wassermann <vis0vis@gmail.com>
2014-02-10 21:26:21 +0100

    changes a setting for precompilation in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,9 @@ StartupDna::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+	#Without this change, the application would serve the precompiled assets from public/assets in development, and you would not see any local changes until you compile assets again.
+	config.assets.prefix = "/dev-assets"
 end
 
 silence_warnings do
