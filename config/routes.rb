2104b141565dde7e095ff2cf91086fbb62fe9ebf
ed wassermann <vis0vis@gmail.com>
2014-02-15 20:58:16 +0100

    Adds route for interviewee_profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 StartupDna::Application.routes.draw do
     
-    root to: 'high_voltage/pages#show', id: 'homepage', via: :get
-	#root to: 'videos#index', via: :get
+  root to: 'high_voltage/pages#show', id: 'homepage', via: :get
 	
+
 	resources :videos,       only: [ :index ]
 	resources :interviewees, only: [ :index, :new, :create ]
+	resources :interviewees_profiles,     only: [:show]
 
-    get 'pages/documentary'      => 'high_voltage/pages#documentary', id: 'documentary'
-    get 'pages/about'      => 'high_voltage/pages#about', id: 'about'
-    get 'pages/contact'      => 'high_voltage/pages#contact', id: 'contact'
+  get 'pages/documentary'      => 'high_voltage/pages#documentary', id: 'documentary'
+  get 'pages/about'      => 'high_voltage/pages#about', id: 'about'
+  get 'pages/contact'      => 'high_voltage/pages#contact', id: 'contact'
 end
