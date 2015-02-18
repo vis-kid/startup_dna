a5478348877a9748e7e37fb4f8aad72658d08dcd
ed wassermann <vis0vis@gmail.com>
2015-02-18 14:52:46 +0100

    Indentation clean up for interviewee controller

--- app/controllers/interviewees_controller.rb
+++ app/controllers/interviewees_controller.rb
@@ -1,22 +1,22 @@
 class IntervieweesController < ApplicationController
 
-	def index
-		@interviewees = Interviewee.order('created_at desc').paging(params[:page]).per(7) 
+  def index
+    @interviewees = Interviewee.order('created_at desc').paging(params[:page]).per(7) 
 	end
 
-	def new
-		@interviewee = Interviewee.new
+  def new
+    @interviewee = Interviewee.new
 	end
 
-	def create
-		@interviewee = Interviewee.create(interviewee_params)
-	  redirect_to interviewees_path
+  def create
+    @interviewee = Interviewee.create(interviewee_params)
+    redirect_to interviewees_path
 	end
 
 	private
 
-	def interviewee_params 
-		params.require(:interviewee).permit(:name, 
+  def interviewee_params 
+    params.require(:interviewee).permit(:name, 
 																				:about_text, 
 																				:twitter_handle, 
 																				:weblink, 
