702e97f2ffa4be2aefced91825499128d89a740d
ed wassermann <vis0vis@gmail.com>
2015-02-18 15:02:59 +0100

    Fixes indentation in interviewee factory

--- spec/factories/interviewees.rb
+++ spec/factories/interviewees.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-	factory :interviewee do
-		name 'Steve'
-		twitter_handle '@steve'
-		weblink 'www.reddit.com'
-		about_text 'He is well known for Reddit'
+  factory :interviewee do
+    name 'Steve'
+    twitter_handle '@steve'
+    weblink 'www.reddit.com'
+    about_text 'He is well known for Reddit'
 	end
 end
