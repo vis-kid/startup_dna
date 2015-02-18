3508ae2522e41ba3199a09626034f040d4ade97e
ed wassermann <vis0vis@gmail.com>
2015-02-18 16:11:34 +0100

    Fixes indentation in spec support dom helpers

--- spec/support/test_DOM_helpers.rb
+++ spec/support/test_DOM_helpers.rb
@@ -1,24 +1,23 @@
 module DomHelpers
 
-	def user_sees_correct_page_title(page_title) 
-		expect(page).to have_title page_title
-	end
+  def user_sees_correct_page_title(page_title)
+    expect(page).to have_title page_title
+  end
 
-	def page_has_correct_data_role(data_role)
-		expect(page).to have_css "[ data-role=#{data_role} ]"
-	end
+  def page_has_correct_data_role(data_role)
+    expect(page).to have_css "[ data-role=#{data_role} ]"
+  end
 
-	def user_sees_content(content)
+  def user_sees_content(content)
     expect(page).to have_content content
-	end
-
-	def user_does_not_see_content(content)
+  end
+  
+  def user_does_not_see_content(content)
     expect(page).not_to have_content content
-	end
-
+  end
 
 end
 
-RSpec.configure do  |config| 
-	config.include DomHelpers
+RSpec.configure do  |config|
+  config.include DomHelpers
 end
