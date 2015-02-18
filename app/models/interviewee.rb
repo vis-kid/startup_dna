608afbff512668f3a984ab1495c063c79b5bf67b
ed wassermann <vis0vis@gmail.com>
2015-02-18 14:58:38 +0100

    Cleans up Interviewee model

--- app/models/interviewee.rb
+++ app/models/interviewee.rb
@@ -1,5 +1,7 @@
 class Interviewee < ActiveRecord::Base
-	has_many :videos
 
+  has_many :videos
+  
 	has_attached_file :user_pic,:styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
 end
