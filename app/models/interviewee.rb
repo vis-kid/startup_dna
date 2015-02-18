3e693691cf86b9d1205a282a43a406899e7eb84a
ed wassermann <vis0vis@gmail.com>
2015-02-18 14:59:55 +0100

    omg, seriously need to look into vim fucking up indentations grrr

--- app/models/interviewee.rb
+++ app/models/interviewee.rb
@@ -2,6 +2,6 @@ class Interviewee < ActiveRecord::Base
 
   has_many :videos
   
-	has_attached_file :user_pic,:styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :user_pic,:styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
 end
