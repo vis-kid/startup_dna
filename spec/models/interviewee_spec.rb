32fe6612d3050e067570bda60ea0bb857ecaf192
ed wassermann <vis0vis@gmail.com>
2015-02-18 15:45:16 +0100

    Fixes indentation in interviewee model spec

--- spec/models/interviewee_spec.rb
+++ spec/models/interviewee_spec.rb
@@ -1,93 +1,92 @@
 require 'spec_helper'
 
 describe Interviewee do
+  context '#name' do
+  
+    it 'responds to #name' do
+      interviewee = named_interviewee
+      expect(interviewee).to respond_to :name
+    end
 
-	context '#name' do
+    it 'returns the correct name' do
+      interviewee = interviewee_named 'Steve Huffmann'
+      expect(interviewee.name).to eq'Steve Huffmann'
+    end
 
-		it 'responds to #name' do
-			interviewee = named_interviewee	
-			expect(interviewee).to respond_to :name
-		end
+  end
 
-		it 'returns the correct name' do
-			interviewee = interviewee_named 'Steve Huffmann'
-			expect(interviewee.name).to eq'Steve Huffmann'
-		end
-	end
-
-	context '#twitter_handle' do
-
-		it 'responds to #twitter_handle' do
+  context '#twitter_handle' do
+    
+    it 'responds to #twitter_handle' do
       interviewee = interviewee_has_twitter_handle
       expect(interviewee).to respond_to :twitter_handle
-		end
-
-		it 'returns the correct twitter_handle' do
-      interviewee = interviewee_with_twitter_handle '@steve_huffmann'  
+    end
+    
+    it 'returns the correct twitter_handle' do
+      interviewee = interviewee_with_twitter_handle '@steve_huffmann'
       expect(interviewee.twitter_handle).to eq '@steve_huffmann'
-		end
-	end
+    end
 
-	context '#weblink' do
+  end
+	
+  context '#weblink' do
 
-		it 'responds to #weblink' do
+    it 'responds to #weblink' do
       interviewee = interviewee_has_weblink
       expect(interviewee).to respond_to :weblink
-		end
-
-		it 'returns the correct weblink' do
+    end
+    
+    it 'returns the correct weblink' do
       interviewee = interviewee_with_weblink 'www.hipmunk.com'
       expect(interviewee.weblink).to eq 'www.hipmunk.com'
-		end
-	end
-	
-	context '#about_text' do
+    end
 
-		it 'responds to #about_text' do
+  end
+	
+  context '#about_text' do
+
+    it 'responds to #about_text' do
       interviewee = interviewee_has_about_text
       expect(interviewee).to respond_to :about_text
-		end
+    end
 
-		it 'returns the correct weblink' do
+    it 'returns the correct weblink' do
       interviewee = interviewee_with_about_text 'He was a co-founder ...'
       expect(interviewee.about_text).to eq 'He was a co-founder ...'
-		end
-	end
+    end
+
+  end
 
 	describe "Interviewee_Video association #videos"do
+    it 'responds to video' do
+      interviewee = basic_interviewee
+      expect(interviewee).to respond_to :videos
+    end
 
-		it 'responds to video' do
-			interviewee = basic_interviewee 
-			expect(interviewee).to respond_to :videos
-		end
+    it 'associates user to user-specific videos' do
+      interviewee = basic_interviewee
+      correct_video = basic_video
+      other_video = basic_video
 
-		it 'associates user to user-specific videos' do
-			interviewee = basic_interviewee 
-			correct_video = basic_video 
-			other_video = basic_video
-
-			interviewee.videos << correct_video
-
-			expect(interviewee.videos).to include correct_video
-			expect(interviewee.videos).not_to include other_video
-			expect(interviewee.videos.length).to eq 1
-
-		end
-	end
+      interviewee.videos << correct_video
+      
+      expect(interviewee.videos).to include correct_video
+      expect(interviewee.videos).not_to include other_video
+      expect(interviewee.videos.length).to eq 1
+    end
+  end
 end
 
-
-
 def basic_interviewee
-	create :interviewee
+  create :interviewee
 end
 
 def basic_video
-	create :video
+  create :video
 end
 
 def interviewee_has_about_text
-	interviewee_with_about_text 'Some about text goes in here'
+  interviewee_with_about_text 'Some about text goes in here'
 end
 
 def interviewee_with_about_text(description)
@@ -103,17 +102,17 @@ def interviewee_with_weblink(weblink)
 end
 
 def interviewee_has_twitter_handle
-	interviewee_with_twitter_handle '@steve_huffmann'
+  interviewee_with_twitter_handle '@steve_huffmann'
 end
 
 def interviewee_with_twitter_handle(twitter_handle)
-	create :interviewee, twitter_handle: twitter_handle
+  create :interviewee, twitter_handle: twitter_handle
 end
 
 def named_interviewee
-	interviewee_named 'Steve Huffmann'
+  interviewee_named 'Steve Huffmann'
 end
 
 def interviewee_named(name)
-	create :interviewee, name: name
+  create :interviewee, name: name
 end
