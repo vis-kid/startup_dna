743a976b67dfa2091040aeea737625a2401eb3a8
ed wassermann <vis0vis@gmail.com>
2015-02-18 22:09:03 +0100

    Fixes indentation for view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-	def page_header(header)
-		content_tag(:div, class: 'page-header-wrapper') do
-			concat(content_tag(:h1, "#{header}", class: 'page-header', 'data-role' => 'page-header')) 
-		end
-	end
+  def page_header(header)
+    content_tag(:div, class: 'page-header-wrapper') do
+      concat(content_tag(:h1, "#{header}", class: 'page-header', 'data-role' => 'page-header'))
+    end
+  end
 end
