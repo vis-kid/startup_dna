4c3d33bf9ea0d1fa7fa84d6f37ebae4d1c428b11
ed wassermann <vis0vis@gmail.com>
2015-02-18 15:06:27 +0100

    Fixes indentation in Video factory

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
-	factory :video do
-		quote 'Stanford was good for me'
+  factory :video do
+    quote 'Stanford was good for me'
     category 'Future Of Education'
-		description 'Once upon a time'
-		youtube_id '12345678910'
-		vimeo_id '12345678910'
-	end
+    description 'Once upon a time'
+    youtube_id '12345678910'
+    vimeo_id '12345678910'
+  end
 end
