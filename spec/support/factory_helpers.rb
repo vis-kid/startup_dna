3c0b3c00c6bdb10c90d1e4cc44e83e6da89ac469
ed wassermann <vis0vis@gmail.com>
2015-02-18 16:08:52 +0100

    Fixes indentation in factory helpers

--- spec/support/factory_helpers.rb
+++ spec/support/factory_helpers.rb
@@ -1,34 +1,33 @@
 ###  INTERVIEWEE
 
 def basic_interviewee
-	create :interviewee
+  create :interviewee
 end
 
 def interviewee_named(name)
-	create :interviewee, name: name
+  create :interviewee, name: name
 end
 
 def interviewee_with_twitter_handle(twitter_handle)
-	create :interviewee, twitter_handle: twitter_handle
+  create :interviewee, twitter_handle: twitter_handle
 end
 
 def interviewee_with_weblink(weblink)
-	create :interviewee, weblink: weblink
+  create :interviewee, weblink: weblink
 end
-
 
 #####  VIDEO
 
 def basic_video
-	create :video
+  create :video
 end
 
 def video_has_a_quote
-	video_with_quote 'Some quote'
+  video_with_quote 'Some quote'
 end
 
 def video_with_quote(quote)
-	create :video, quote: quote
+  create :video, quote: quote
 end
 
 def video_has_category
@@ -36,13 +35,13 @@ def video_has_category
 end
 
 def video_with_category(category)
-	create :video, category: category
+  create :video, category: category
 end
 
 def video_has_description
-	video_with_description 'Some awesome description'
+  video_with_description 'Some awesome description'
 end
 
 def video_with_description(description)
-	create :video, description: description
+  create :video, description: description
 end
